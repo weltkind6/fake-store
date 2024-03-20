09a8c8db940e81e860f7277863070ce42508bfa4
yaroslavzefi <yaroslavzef@gmail.com>
2024-03-20 20:56:38 +0300

    add: webpack dev server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 console.log('Hellow world!')
+
+const root = document.getElementById('root')
+
+root.textContent = 'abracadabra'
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,26 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-export default (env: any) => {
+type Mode = 'production' | 'development'
+
+interface EnvVariables {
+  mode: Mode
+}
+
+export default (env: EnvVariables) => {
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+    ],
     module: {
       rules: [
         {
@@ -18,14 +33,10 @@ export default (env: any) => {
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
     },
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "[name].[contenthash].js",
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-    ],
+    devServer: {
+       port: 3000,
+       open: true
+    }
   };
 
   return config
