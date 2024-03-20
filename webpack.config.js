58b901ca3f971012a3746580342d99a9bd843612
yaroslavzefi <yaroslavzef@gmail.com>
2024-03-20 10:38:01 +0300

    add: webpack basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log("Goal: ", env.goal); // 'local'
+  console.log("Production: ", env.production); // true
+
+  return {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
+    ],
+  };
+};
