39f890d81d18769de369a70d5ef2f92a6a3c4e5e
yaroslavzefi <yaroslavzef@gmail.com>
2024-03-21 18:41:08 +0300

    refactor: webpack config decomposition

--- webpack.config.ts
+++ webpack.config.ts
@@ -11,45 +11,68 @@ interface EnvVariables {
   port: number
 }
 
+const getEntryConfig = () => {
+  return path.resolve(__dirname, "src", "index.tsx");
+};
+
+const getOutputConfig = () => {
+  return {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  };
+};
+
+const getPluginsConfig = () => {
+  return [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    })
+  ];
+};
+
+const getModuleConfig = () => {
+  return {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  };
+};
+
+const getResolveConfig = () => {
+  return {
+    extensions: ['.tsx', '.ts', '.js'],
+  };
+};
+
+const getDevServerConfig = (env: EnvVariables) => {
+  return {
+    port: env.port ?? 3000,
+    open: true
+  };
+};
+
 export default (env: EnvVariables) => {
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, "src", "index.tsx"),
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "[name].[contenthash].js",
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-      new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css'
-      })
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-        },
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
+    entry: getEntryConfig(),
+    output: getOutputConfig(),
+    plugins: getPluginsConfig(),
+    module: getModuleConfig(),
+    resolve: getResolveConfig(),
     devtool: 'inline-source-map',
-    devServer: {
-       port: env.port ?? 3000,
-       open: true
-    }
+    devServer: getDevServerConfig(env)
   };
 
-  return config
+  return config;
 };
-
